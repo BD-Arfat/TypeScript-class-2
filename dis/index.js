b566f971b7a5960b591a33cc03d4dee4eb3de482
BD-Arfat <arfat1islam5162@gmail.com>
2023-10-19 11:35:53 +0600

    first commit

---
+++ dis/index.js
@@ -0,0 +1,9 @@
+"use strict";
+const courst = 'hiiii hellloooooo';
+console.log(courst);
+const nam = 'fotu vai ken acho';
+console.log(nam);
+const num = 999;
+console.log(num);
+const isMarid = true;
+console.log(isMarid);
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+const courst: string = 'hiiii hellloooooo';
+console.log(courst)
+
+const nam: string = 'fotu vai ken acho'
+console.log(nam);
+
+const num: number = 999;
+console.log(num)
+
+const isMarid: boolean = true;
+console.log(isMarid)

1fbb84ce2e31fc21fa207ec1e12fa03359b8dcb2
BD-Arfat <arfat1islam5162@gmail.com>
2023-10-23 21:37:23 +0600

    add

--- src/class-6.ts
+++ src/class-6.ts
@@ -1,8 +1,12 @@
 var string1:string[]=['arfat', 'rifat','shifat', 'lofat'];
 var string2:string[]=['maruf','karup','tarup','carup'];
 
+
+var [str1, str2, str3]=string1;
+console.log(str1, str2, str3)
+
 string1.push(...string2);
-console.log(string1);
+// console.log(string1);
 
 var herScore = (score:number):number=>{
     return score;
@@ -17,4 +21,12 @@ var sumNumbers = (...sumNumber:number[]):number=>{
     })
     return sum;
 }
-console.log(sumNumbers(1,8,5,4,9,5,8))
+// console.log(sumNumbers(1,8,5,4,9,5,8));
+
+var heroStrings = (...heroString:string[]):void =>{
+
+    // heroString.map(stn => console.log(stn))
+   
+};
+
+heroStrings('arfat', 'lisa', 'tisha', 'sana', 'rifat', 'saoud')
---
+++ src/class-7.ts
@@ -0,0 +1,6 @@
+
+type heroScoreType = number|string|boolean;
+
+var heroScore:heroScoreType = 'arfat' 
+var heroNumber:heroScoreType = 55
+var isHero:heroScoreType = true;
